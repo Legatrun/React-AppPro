63831d8e57213ae090bf694b5c6238b6fdf024fc
Joaquim Merlo <joaquimmerlo1@gmail.com>
2022-10-10 18:55:32 -0400

    form - components -fields - edde

---
+++ src/03-forms/pages/FormikAbstraction.tsx
@@ -0,0 +1,87 @@
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+
+import { MyCheckbox, MySelect, MyTextInput } from '../components'
+
+import '../styles/styles.css'
+
+export const FormikAbstractations = () => {
+
+    return (
+        <div>
+            <h1>Formik Abstractation</h1>
+
+            <Formik
+                initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    terms: false,
+                    jobType: '',
+
+                }}
+                onSubmit={(values) => {
+                    console.log(values)
+                }}
+                validationSchema={Yup.object({
+                    firstName: Yup.string()
+                        .max(15, 'Debe de tener 15 caracteres o menos')
+                        .required('Requerido')
+                    ,
+                    lastName: Yup.string()
+                        .max(15, 'Debe de tener 15 caracteres o menos')
+                        .required('Requerido')
+                    ,
+                    email: Yup.string()
+                        .email('El correo no tiene un formato válido')
+                        .required('Requerido'),
+                    terms: Yup.boolean()
+                        .oneOf([true], 'Debe de aceptar las condiciones'),
+                    jobType: Yup.string()
+                        .required('Requerido')
+                        .notOneOf(['it-jr'], 'Esta opcion no es permitida')
+                })}
+            >
+                {
+                    (formik) => (
+                        <Form>
+                            <MyTextInput
+                                label='First Name'
+                                name='firstName'
+                                placeholder='Jair'
+                            />
+
+                            <MyTextInput
+                                label='Last Name'
+                                name='lastName'
+                                placeholder='Merlo'
+                            />
+
+                            <MyTextInput
+                                label='Email'
+                                name='email'
+                                placeholder='ejemplo@google.com'
+                            />
+
+                            <MySelect label="Job Type" name="jobType" >
+                                <option value="">Pick something</option>
+                                <option value="developer">Developer</option>
+                                <option value="designer">Designer</option>
+                                <option value="it-senior">IT Senior</option>
+                                <option value="it-jr">IT Jr.</option>
+                            </MySelect>
+
+                            <MyCheckbox label="Terms & Conditions" name="terms" />
+
+                            <button type='submit'>Submit</button>
+
+                        </Form>
+                    )
+                }
+            </Formik>
+
+
+
+        </div>
+    )
+}
--- src/routes/Navitagion.tsx
+++ src/routes/Navitagion.tsx
@@ -1,5 +1,12 @@
 import { BrowserRouter } from "react-router-dom"
 import { Routes, Route, NavLink, Navigate } from "react-router-dom"
+import {
+    FormikAbstractations,
+    FormikBasicPage,
+    FormikComponents,
+    FormikYupPage,
+    RegisterPage
+} from "../03-forms/pages"
 
 import react from '../assets/react.svg'
 
@@ -11,18 +18,29 @@ export const Navitagion = () => {
                     <img src={react} alt="React logo" />
                     <ul>
                         <li>
-                            <NavLink to="/home" className={({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink>
+                            <NavLink to="/register" className={({ isActive }) => isActive ? 'nav-active' : ''}>Register Page</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
+                            <NavLink to="/formik-basic" className={({ isActive }) => isActive ? 'nav-active' : ''}>FormikBasicPage</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
+                            <NavLink to="/formik-yup" className={({ isActive }) => isActive ? 'nav-active' : ''}>formik yup</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/formik-components" className={({ isActive }) => isActive ? 'nav-active' : ''}>formik components</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/formik-abstractation" className={({ isActive }) => isActive ? 'nav-active' : ''}>formik abstractation</NavLink>
                         </li>
                     </ul>
                 </nav>
 
                 <Routes>
+                    <Route path="register" element={<RegisterPage />} />
+                    <Route path="formik-basic" element={<FormikBasicPage />} />
+                    <Route path="formik-yup" element={<FormikYupPage />} />
+                    <Route path="formik-components" element={<FormikComponents />} />
+                    <Route path="formik-abstractation" element={<FormikAbstractations />} />
                     <Route path="about" element={<h1>About Page</h1>} />
                     <Route path="users" element={<h1>Users Page</h1>} />
                     <Route path="home" element={<h1>Home Page</h1>} />
